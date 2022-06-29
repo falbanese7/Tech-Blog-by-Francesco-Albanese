b36f1ebbc0ebd765b9bbe8df6fc03e49fe41f3fd
Francesco Albanese <falbanese96@gmail.com>
2022-06-28 22:59:34 -0400

    frontend javascript

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const commentFormHandler = async (event) => {
+  event.preventDefault();
+
+  const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
+  const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+
+  if (comment_text) {
+    const response = await fetch('/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({ comment_text, post_id }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+      document.querySelector('#comment-form').style.display = 'block';
+    }
+  }
+};
+document
+  .getElementById('#comment-form')
+  .addEventListener('submit', commentFormHandler);
